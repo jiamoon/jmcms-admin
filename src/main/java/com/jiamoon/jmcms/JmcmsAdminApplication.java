0fe7ffbce322f53b4aa806c1af05ec4cde9f56b1
jiamoon@aliyun.com <jiamoon@aliyun.com>
2018-07-16 11:52:32 +0800

    redis工具类

--- src/main/java/com/jiamoon/jmcms/JmcmsAdminApplication.java
+++ src/main/java/com/jiamoon/jmcms/JmcmsAdminApplication.java
@@ -1,15 +1,16 @@
 package com.jiamoon.jmcms;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @EnableAutoConfiguration
+@MapperScan({"com.jiamoon.jmcms.modules", "com.jiamoon.jmcms.common.util", "com.jiamoon.jmcms.common.service"})
 @SpringBootApplication
 public class JmcmsAdminApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JmcmsAdminApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JmcmsAdminApplication.class, args);
+    }
 }
--- src/main/java/com/jiamoon/jmcms/modules/admin/controller/IndexController.java
+++ src/main/java/com/jiamoon/jmcms/modules/admin/controller/IndexController.java
@@ -1,17 +1,64 @@
 package com.jiamoon.jmcms.modules.admin.controller;
 
 import com.jiamoon.jmcms.common.controller.BaseController;
+import com.jiamoon.jmcms.common.util.RedisUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * 后台首页控制层
  */
 @Controller
-public class IndexController extends BaseController{
+public class IndexController extends BaseController {
+    @Autowired
+    RedisUtils redisUtils;
 
     @RequestMapping("")
     public String index() {
         return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping("redis")
+    public Object redis(String key, String value) {
+        //redisUtils.set(key,value,10L, TimeUnit.MINUTES);
+        List<HashMap<Object, Object>> list = new ArrayList<>();
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("title", "灭世");
+        map.put("time", new Date());
+        list.add(map);
+        map = new HashMap<>();
+        map.put("title", "杀戮");
+        map.put("time", new Date());
+        list.add(map);
+        return redisUtils.get(key);
+    }
+
+    @ResponseBody
+    @RequestMapping("redisListPush")
+    public Object redisListPush(String key) {
+        //redisUtils.set(key,value,10L, TimeUnit.MINUTES);
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("title", "灭世");
+        map.put("time", new Date());
+        redisUtils.lPush(key, map);
+        return redisUtils.lRange(key, 0, -1);
+    }
+    @ResponseBody
+    @RequestMapping("redisMap")
+    public Object redisMap(String key,String hashKey,String value) {
+        //redisUtils.set(key,value,10L, TimeUnit.MINUTES);
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put("title", "灭世");
+        map.put("time", new Date());
+        redisUtils.hashSet(key,hashKey,value);
+        return redisUtils.hashGet(key,hashKey);
     }
 }
